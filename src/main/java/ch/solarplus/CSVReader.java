e8867322a3c668ce4d099f2f0f82d7c00a89c196
ivan.tujkic <ivan.tujkic@3s-solarplus.ch>
2021-12-01 10:05:07 +0100

    connect with MySQL

--- src/main/java/ch/solarplus/CSVReader.java
+++ src/main/java/ch/solarplus/CSVReader.java
@@ -1,49 +1,98 @@
 package ch.solarplus;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CSVReader {
 
-    public static final String DELIMETER = ",";
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        
+        // URL from the DB "grades"
+        String jdbcURL = "jdbc:mysql://localhost:3306/grades";
 
-    public static void main(String[] args) {
+        // change the username
+        String username = "*****";
 
-        // csv file to read
-        String csvFile = "C:\\Users\\ivan.tujkic\\Documents\\Summaries\\VC Code\\grades.csv";
-        CSVReader.readCSV(csvFile);
-
-    }
-
-    public static void readCSV(String csvFile) {
-
+        // change the password
+        String password = "";
+ 
+        String csvFilePath = "C:\\Users\\ivan.tujkic\\Documents\\Summaries\\VC Code\\grades.csv";
+ 
+        int batchSize = 20;
+ 
+        Connection connection = null;
+ 
         try {
-            File file = new File(csvFile);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader buffReader = new BufferedReader(fileReader);
-            String line = "";
-            String[] tempArr;
-            
-            while ((line = buffReader.readLine()) != null) {
-
-                // splitting all data by ,
-                tempArr = line.split(DELIMETER);
-                for (String tempStr : tempArr) {
-
-                    // print and replace all "" with nothing
-                    System.out.print(tempStr.replaceAll("\"", ""));
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+            connection.setAutoCommit(false);
+ 
+            // insert-query for the table
+            String sql = 
+                "INSERT INTO grades_student (lastname, firstname, test_1, test_2, test_3, test_4, final_grade, grade) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+ 
+            BufferedReader lineReader = new BufferedReader(new FileReader(csvFilePath));
+            String lineText = null;
+ 
+            int count = 0;
+ 
+            // skip header line
+            lineReader.readLine(); 
+ 
+            while ((lineText = lineReader.readLine()) != null) {
+                String[] data = lineText.split(",");
+                String lastname = data[0];
+                String firstname = data[1];
+                String test_1 = data[2];
+                String test_2 = data[3];
+                String test_3 = data[4];
+                String test_4 = data[5];
+                String final_grade = data[6];
+                String grade = data[7];
+ 
+                stmt.setString(1, lastname);
+                stmt.setString(2, firstname);
+                stmt.setString(3, test_1);
+                stmt.setString(4, test_2);
+                stmt.setString(5, test_3);
+                stmt.setString(6, test_4);
+                stmt.setString(7, final_grade);
+                stmt.setString(8, grade);
+ 
+                stmt.addBatch();
+ 
+                if (count % batchSize == 0) {
+                    stmt.executeBatch();
                 }
-                System.out.println();
             }
-
-            buffReader.close();
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+ 
+            lineReader.close();
+ 
+            // execute the remaining queries
+            stmt.executeBatch();
+ 
+            connection.commit();
+            connection.close();
+ 
+        } catch (IOException ex) {
+            System.err.println(ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+ 
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            
         }
-
+ 
     }
 
 }
